55554b5694980335fc3411999edffb4fc4a232bc
Roosevelt <panta_85.x@hotmail.com>
2018-03-22 16:49:50 -0500

    set datetime at tally constructor

--- Dtos/TallyForCreationDto.cs
+++ Dtos/TallyForCreationDto.cs
@@ -8,5 +8,9 @@ namespace caja.Dtos
         public decimal Final { get; set; }
         public int UserId { get; set; }
         public int TillId { get; set; }
+        public TallyForCreationDto()
+        {
+            this.Created = DateTime.Now;
+        }
     }
 }
